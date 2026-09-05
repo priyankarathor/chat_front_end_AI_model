c3fe97dfadf916fedfc87a2feaffaf27adc461ed
priyankarathor <priyankaji856@gmail.com>
2026-09-05 23:46:02 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/update
 export default defineConfig({
   plugins: [react()],
   server: {
