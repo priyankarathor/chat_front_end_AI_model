46a01e6fe312ab8081eb51312c839fccda23020b
priyankarathor <priyankaji856@gmail.com>
2026-09-05 23:59:56 +0530

    all set

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/update
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://chat-backend-ai-model.vercel.app/',
+        target: 'https://chat-backend-ai-model.vercel.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
